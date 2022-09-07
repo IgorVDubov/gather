da4286f7cd58d8161a9c14725d7fa375c36a21af
Igor Dubov <4dubov@gmail.com>
2022-09-07 18:36:14 +0300

    dd

--- sandbox.py
+++ sandbox.py
@@ -1,30 +1,14 @@
-from operator import attrgetter
-import classes 
-import channel_handlers
+from dataclasses import dataclass
+
+@dataclass
+class A():
+    i:int
+    s:str
+
+@dataclass    
+class B(A):
+    c:int
 
 
-
-class Node:
-    def __init__(self,id,result) -> None:
-        self.id=id
-        self.result = result
-        self.resultOut = None
-    def __str__(self) -> str:
-        return f'Node {self.id=}, {self.result=}, {self.resultOut=}'
-
-
-
-
-def handler1(vars, stored):
-    print (f'in handler: {vars.ch1=}, {vars.result=}, {stored.a=}')
-    vars.result=vars.ch1
-    stored.a=5
-    print (f'exit handler: {vars.ch1=}, {vars.result=}, {stored.a=}')
-    return stored
-
-
-
-
-
-
-
+a=B(s='xsd',i=1,c=44)
+print(a)
