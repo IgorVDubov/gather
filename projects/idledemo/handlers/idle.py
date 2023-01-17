c6671e809001af71481945856819d90dfb6f61be
IgorVDubov <4dubov@gmail.com>
2023-01-17 12:40:44 +0300

    idle production

---
+++ projects/idledemo/handlers/idle.py
@@ -0,0 +1,2 @@
+from ..logics import current_idle_store, current_idle_reset, current_idle_add_cause
+
--- projects/idledemo/logics.py
+++ projects/idledemo/logics.py
@@ -1,4 +1,11 @@
+import importlib
+from dataclasses import dataclass
+from datetime import datetime
+
+import channels.channelbase
 import globals
+
+settings=importlib.import_module('projects.'+globals.PROJECT['path']+'.settings')
 
 
 def get_machine_from_user(user:str)->int:
@@ -10,8 +17,33 @@ def get_machine_from_user(user:str)->int:
 def get_machine_causes(id:int)->dict[int:str]:               # TODO refact
     return globals.idle_causes
 
-def get_current_state(id:int)->dict['machine':id, 'state':int, 'begin_time':str, 'couse_id':int]:
-    state=2
-    begin_time='2023-01-10 08:20:00'
-    cause_id=1
-    return {'machine':id, 'state':state, 'begin_time':begin_time, 'cause_id':cause_id}
+def get_current_state(channel_base, id:int)->dict['machine':id, 'state':int, 'begin_time':str, 'couse_id':int]:
+    channel=channel_base.get(id)
+
+    state=channel.get_arg(settings.STATE_ARG)
+    begin_time=channel.get_arg(settings.STATE_TIME_ARG).strftime('%Y-%m-%dT%H:%M:%S')
+    cause_id=0
+    cause_time=begin_time
+    return {'machine':id, 'state':state, 'begin_time':begin_time, 'cause_id':cause_id, 'cause_time':cause_time}
+
+@dataclass
+class Idle():
+    state:int
+    begin_time:datetime
+    cause:int=None
+    cause_time:datetime=None
+
+def current_idle_store(machine_id,state):
+    # begin_time=datetime.now().strftime(('%Y-%m-%dT%H:%M:%S'))
+    begin_time=datetime.now()
+    globals.machines_idle.update({machine_id:Idle(state,begin_time)})
+
+def current_idle_reset(machine_id):
+    globals.machines_idle.update({machine_id:None})
+
+def current_idle_add_cause(machine_id,cause_id,cause_time):
+    if globals.machines_idle.get(id):
+        globals.machines_idle.get(id).cause=cause_id
+        globals.machines_idle.get(id).cause_time=cause_time
+    else:
+        raise KeyError(f'no machine {machine_id} in globals.machines_idle')
